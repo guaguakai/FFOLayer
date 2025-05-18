3f2f1a7f3b43a74165c751559931100b3fc0c10a
Kai Wang <kwang692@login-phoenix-rh9-3.pace.gatech.edu>
2025-05-18 00:08:14 -0400

    updated results

--- scripts/exp1.sh
+++ scripts/exp1.sh
@@ -1,9 +1,9 @@
 EPS=0.1
-LR=0.001
+LR=0.0001
 
-for YDIM in  10 20 50 100 200 500
+for YDIM in  10 20 50 100 # 200 500
 do
-	for METHOD in ffoqp # cvxpylayer qpth
+	for METHOD in ffoqp cvxpylayer qpth
 	do
         for SEED in {1..10}
         do
