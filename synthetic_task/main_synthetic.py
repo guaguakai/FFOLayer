02571fcf2238c493b7e931c68f4b19773d23d542
Shing Hei Ho <sho73@kings.cc.gatech.edu>
2025-11-04 14:26:21 -0500

    synthetic task code (new folder)

---
+++ synthetic_task/main_synthetic.py
@@ -0,0 +1,158 @@
+import numpy as np
+import torch
+import time
+import sys
+import os
+import argparse
+
+from models import *
+from data import *
+
+
+from torch.utils.tensorboard import SummaryWriter
+
+
+       
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--method', type=str, help='bilevel layer method')
+    parser.add_argument('--epochs', type=int, default=20, help='number of epochs')
+    parser.add_argument('--seed', type=int, default=3, help='random seed')
+    parser.add_argument('--lr', type=float, default=0.00001, help='learning rate')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    parser.add_argument('--ydim', type=int, default=200, help='dimension of y')
+    
+    parser.add_argument('--alpha', type=float, default=100, help='alpha')
+    parser.add_argument('--dual_cutoff', type=float, default=1e-3, help='dual cutoff')
+    parser.add_argument('--slack_tol', type=float, default=1e-8, help='slack tolerance')
+
+    parser.add_argument('--device', type=str, default='cuda:0', help='device')
+    parser.add_argument('--learn_constraint', type=int, default=0, help='whether to learn constraint')
+    
+    args = parser.parse_args()
+
+    method = args.method
+    seed = args.seed
+    num_epochs = args.epochs
+    learning_rate = args.lr
+    alpha = args.alpha
+    dual_cutoff = args.dual_cutoff
+    slack_tol = args.slack_tol
+
+    # Set random seed for reproducibility
+    device = torch.device(args.device) if torch.cuda.is_available() else torch.device('cpu')
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
+
+    input_dim   = 640
+    ydim  = args.ydim
+    n = ydim
+    num_samples = 2000 #2048
+    batch_size = args.batch_size
+
+    train_loader, test_loader = genData(device, input_dim, ydim, num_samples, batch_size)
+
+    model = OptModel(input_dim, ydim, layer_type=method, constraint_learnable=(args.learn_constraint==1), batch_size=batch_size, device=device, alpha=alpha, dual_cutoff=dual_cutoff, slack_tol=slack_tol).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0)
+    loss_fn = torch.nn.MSELoss()
+    
+    time_str = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+    writer = SummaryWriter()
+
+    deltas = [torch.zeros_like(parameter) for parameter in model.parameters()]
+    gradients = [torch.zeros_like(parameter) for parameter in model.parameters()]
+
+    # Note: the current version only works for the CVXPY solver
+    # solver = QPSolvers.PDIPM_BATCHED
+
+    directory = '../synthetic_results_{}/{}/'.format(args.batch_size, method)
+    filename = '{}_ydim{}_lr{}_seed{}.csv'.format(method, ydim, learning_rate, seed)
+    if os.path.exists(directory + filename):
+        os.remove(directory + filename)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    
+    
+    ts_weight = 0
+    norm_weight = 0
+    with open(directory + filename, 'w') as file:
+        file.write('epoch, train_ts_loss, test_ts_loss, train_df_loss, test_df_loss, forward_time, backward_time\n')
+        file.flush()
+        
+        for epoch in range(num_epochs):
+            print(f"##### epoch {epoch}: ")
+            
+            train_ts_loss_list, test_ts_loss_list = [], []
+            train_df_loss_list, test_df_loss_list = [], []
+            forward_time = 0
+            backward_time = 0
+
+            model.train()
+            for i, (x, y) in enumerate(train_loader):
+                if i%10==0:
+                    print(f"\t\t train example: {i}/{len(train_loader)}")
+                
+                iter_start_time = time.time()
+                
+                start_time = time.time()
+
+                z, y_pred = model(x) # (opt solution, predicted q)
+                ts_loss = loss_fn(y_pred, y)
+                
+                df_loss = torch.mean(y * z)
+                loss = df_loss + ts_loss * ts_weight + torch.norm(z) * norm_weight
+                
+                forward_time += time.time() - start_time
+
+                start_time = time.time()
+                loss.backward()
+                backward_time += time.time() - start_time
+                
+                iter_time = time.time() - iter_start_time
+
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=2.0)
+                #if epoch > 0:
+                optimizer.step()
+
+                optimizer.zero_grad()
+
+                train_ts_loss_list.append(ts_loss.item())
+                train_df_loss_list.append(df_loss.item())
+                
+                print(f"train loss: {loss.item()}, iter time: {iter_time}")
+
+            print('Forward time {}, backward time {}'.format(forward_time, backward_time))
+
+            model.eval()
+            with torch.no_grad():
+                for i, (x, y) in enumerate(test_loader):
+                    z, y_pred = model(x) #opt solution, predicted q
+                    ts_loss = loss_fn(y_pred, y)
+                    df_loss = torch.mean(y * z)
+
+                    test_ts_loss_list.append(ts_loss.item())
+                    test_df_loss_list.append(df_loss.item())
+
+            train_ts_loss = np.mean(train_ts_loss_list)
+            train_df_loss = np.mean(train_df_loss_list)
+            test_ts_loss = np.mean(test_ts_loss_list)
+            test_df_loss = np.mean(test_df_loss_list)
+            print("Epoch {}, Train TS Loss {}, Test TS Loss {}, Train DF Loss {}, Test DF Loss {}".format(epoch, train_ts_loss, test_ts_loss, train_df_loss, test_df_loss))
+
+            writer.add_scalar('Loss/TS/train', train_ts_loss, epoch)
+            writer.add_scalar('Loss/TS/test', test_ts_loss, epoch)
+            writer.add_scalar('Loss/DF/train', train_df_loss, epoch)
+            writer.add_scalar('Loss/DF/test', test_df_loss, epoch)
+
+            file.write('{},{},{},{},{},{},{}\n'.format(epoch, train_ts_loss, test_ts_loss, train_df_loss, test_df_loss, forward_time, backward_time))
+            file.flush()
+            writer.flush()
+            
+        file.close()
+
---
+++ synthetic_task/models.py
@@ -0,0 +1,194 @@
+import torch
+import torch.nn as nn
+import cvxpy as cp
+from constants import *
+
+from torch.nn.parameter import Parameter
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+
+from ffocp_eq import BLOLayer
+from ffocp_eq_multithread import BLOLayer as BLOLayerMT
+
+from qpthlocal.qp import QPFunction
+from cvxpylayers.torch import CvxpyLayer
+from cvxpylayers_local.cvxpylayer import CvxpyLayer as LPGDLayer
+
+import ffoqp_eq_cst
+import ffoqp_eq_cst_schur
+import ffoqp_eq_cst_parallelize
+import ffoqp_eq_cst_pdipm
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim=64, output_dim=10):
+        super(MLP, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.batch_norm1 = nn.BatchNorm1d(128)
+        self.fc2 = nn.Linear(128, 128)
+        self.batch_norm2 = nn.BatchNorm1d(128)
+        self.fc3 = nn.Linear(128, output_dim)
+        self.activation = nn.ReLU()
+        self.bound = 10
+
+    def forward(self, x):
+        x = x.view(-1, self.input_dim)
+        # x = self.activation(self.fc1(x))
+        x = self.activation(self.batch_norm1(self.fc1(x)))
+        # x = self.activation(self.fc2(x))
+        x = self.activation(self.batch_norm2(self.fc2(x)))
+        x = torch.clamp(self.fc3(x), min=-self.bound, max=self.bound)
+        return x
+    
+    
+def setup_cvxpy_synthetic_problem(n):
+    n_ineq_constraints = 2 * n + 1
+    Q_cp = cp.Parameter((n, n), PSD=True)
+    q_cp = cp.Parameter(n)
+    G_cp = cp.Parameter((n_ineq_constraints, n))
+    h_cp = cp.Parameter(n_ineq_constraints)
+    z_cp = cp.Variable(n)
+
+    objective_fn = 0.5 * cp.sum_squares(Q_cp @ z_cp) + q_cp.T @ z_cp
+    constraints = [G_cp @ z_cp <= h_cp]
+
+    problem = cp.Problem(cp.Minimize(objective_fn), constraints)
+    assert problem.is_dpp()
+    
+    parameters = [Q_cp, q_cp, G_cp, h_cp]
+    variables = [z_cp]
+
+    return problem, objective_fn, constraints, parameters, variables
+
+def get_feasible_h(G, z0, s0):
+    '''
+    get a vector h such that the inequality constraint Gy<=h can be satisfied
+    
+    i.e. return h = G(z0) + s0 where s0 are all positive
+    
+    Args:
+        - G: (num_ineq, y_dim)
+        - s0: (num_ineq, )
+        - z0: (y_dim,)
+    '''
+    assert(not torch.any(s0<0))
+    return torch.matmul(G, z0) + s0
+
+class OptModel(nn.Module):
+    def __init__(self, input_dim, opt_dim, layer_type, constraint_learnable, device, batch_size, alpha=100, dual_cutoff=1e-3, slack_tol=1e-6, ):
+        '''
+        The architecture is {parameter - optLayer}.
+            
+        Args:
+            - delta = 1/alpha, which is the perturbation constant for finite difference
+        '''
+        super().__init__()
+        self.layer_type = layer_type
+        assert(layer_type in [FFOCP_EQ, CVXPY_LAYER, LPGD, QPTH, LPGD_QP, FFOQP_EQ, FFOQP_EQ_SCHUR, FFOQP_EQ_PARALLELIZE, FFOQP_EQ_PDIPM])
+        
+        self.y_dim = opt_dim
+        self.input_dim = input_dim
+        self.num_ineq = 2 * opt_dim + 1
+        self.num_eq = 0
+        
+        self.constraint_learnable = constraint_learnable
+        
+        #### predictor of parameters
+        # if constraint_learnable:
+        #     self.predictor = MLP(input_dim, self.y_dim + self.num_ineq*self.y_dim + self.y_dim + self.num_ineq)
+        # else:
+        self.predictor = MLP(input_dim, self.y_dim)
+        
+        ### default optimization parameters
+        self.Q = torch.eye(opt_dim).to(device)#.double()
+        G = torch.cat([torch.eye(opt_dim), -torch.eye(opt_dim), torch.ones(1,opt_dim)], dim=0)#.double()
+        h = torch.cat([torch.zeros(opt_dim), torch.ones(opt_dim), torch.Tensor([3])], dim=0)#.double()
+        self.A = torch.Tensor().to(device)
+        self.b = torch.Tensor().to(device)
+        
+        ##### learnable constraints
+        if constraint_learnable:
+            self.G = Parameter(torch.rand((self.num_ineq, self.y_dim)))
+            self.z0_g = Parameter(torch.zeros((self.y_dim,)))
+            self.log_s0 = Parameter(torch.rand((self.num_ineq,)))
+            
+        else:
+            self.G = G.to(device)
+            self.h = h.to(device)
+            
+        if self.layer_type not in [QPTH, LPGD_QP]:
+            problem, objective_fn, constraints, params, variables = setup_cvxpy_synthetic_problem(opt_dim)
+    
+            multithread = True
+            if layer_type==FFOCP_EQ:
+                if not multithread:
+                    self.optlayer = BLOLayer(problem, parameters=params, variables=variables, alpha=alpha, dual_cutoff=dual_cutoff, slack_tol=slack_tol)
+                else:
+                    problem_list = []
+                    params_list = []
+                    variables_list = []
+                    for i in range(batch_size):
+                        problem, objective_fn, constraints, params, variables = setup_cvxpy_synthetic_problem(opt_dim)
+                        problem_list.append(problem)
+                        params_list.append(params)
+                        variables_list.append(variables)
+                    
+                    self.optlayer = BLOLayerMT(problem_list, parameters_list=params_list, variables_list=variables_list, alpha=alpha, dual_cutoff=dual_cutoff, slack_tol=slack_tol)
+                    
+            elif layer_type==CVXPY_LAYER:
+                self.optlayer = CvxpyLayer(problem, parameters=params, variables=variables)
+            elif layer_type==LPGD:
+                self.optlayer = LPGDLayer(problem, parameters=params, variables=variables, lpgd=True)
+            
+            elif layer_type==FFOQP_EQ:
+                self.optlayer = ffoqp_eq_cst.ffoqp(alpha=alpha, chunk_size=1)
+            elif layer_type == FFOQP_EQ_PDIPM:
+                self.optlayer = ffoqp_eq_cst_pdipm.ffoqp(alpha=alpha)
+            elif layer_type == FFOQP_EQ_SCHUR: ## use this ffoqp_cst
+                self.optlayer = ffoqp_eq_cst_schur.ffoqp(alpha=alpha, chunk_size=1)
+            elif layer_type == FFOQP_EQ_PARALLELIZE:
+                self.optlayer = ffoqp_eq_cst_parallelize.ffoqp(alpha=alpha, chunk_size=1)
+        else:
+            if self.layer_type==QPTH:
+                self.optlayer = QPFunction(verbose=-1)
+                
+                
+    def forward(self, x):
+        nBatch = x.size(0)
+        x = x.view(nBatch, -1) #(B, input_dim)
+        
+        out = self.predictor(x)
+        q_pred = out[..., :self.y_dim]
+        
+        if self.constraint_learnable:
+            h = get_feasible_h(self.G, self.z0_g, torch.exp(self.log_s0))
+        else:
+            h = self.h
+            
+        if self.layer_type in [QPTH, FFOQP_EQ, FFOQP_EQ_SCHUR, FFOQP_EQ_PARALLELIZE, FFOQP_EQ_PDIPM]:
+            sol = self.optlayer(
+                self.Q, q_pred, self.G, h, self.A, self.b
+            )
+        else:
+            # Expand constant params along batch dimension
+            Q_batched = self.Q.unsqueeze(0).expand(nBatch, -1, -1)   # (batch, y_dim, y_dim)
+            G_batched = self.G.unsqueeze(0).expand(nBatch, -1, -1)   # (batch, num_ineq, y_dim)
+            h_batched = h.unsqueeze(0).expand(nBatch, -1)       # (batch, num_ineq)
+            
+            params_batched = [Q_batched, q_pred, G_batched, h_batched]
+            
+            if self.layer_type==LPGD:
+                sol, = self.optlayer(*params_batched, solver_args={"eps": 1e-12}) #, solver_args={"eps": 1e-8, "max_iters": 10000, "acceleration_lookback": 0}
+            else:
+                sol, = self.optlayer(*params_batched)
+                
+        return sol, q_pred
+        
+        
+        
